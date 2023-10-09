21d8857fc638f703e579cd4d0580609e1221cb48
ExostFlash <120869320+ExostFlash@users.noreply.github.com>
2023-10-09 10:29:01 +0200

    Update index.php

--- Maizy_A/index.php
+++ Maizy_A/index.php
@@ -1,44 +1,29 @@
 <?php
 
+define("LANG_FR", "FR");
+define("LANG_EN", "EN");
+
 $link_bd = $_SERVER['DOCUMENT_ROOT'] . "/SauvegardeDB/dbb.php";
 $nom_de_la_base = "BioLink";
 
 // page
+$id_post_page = isset($_GET['id_post']) ? $_GET['id_post'] : null;
+$page = isset($_GET['page']) ? $_GET['page'] : "about";
+$lang = isset($_GET['lang']) ? $_GET['lang'] : LANG_FR;
 
-if (isset($_GET['id_post'])) {
-	$id_post_page = $_GET['id_post'];
-}
-
-if (isset($_GET['page'])) {
-	$page = $_GET['page'];
-} else {
-	$page = "about";
-}
-
-if (isset($_GET['lang'])) {
-	$lang = $_GET['lang'];
-} else {
-	$lang = "FR";
-}
-// import +
-
+// import
 include($link_bd);
 include("function.php");
 
 session_start();
 
-?>
+// import page
+include(__DIR__ . "/dark/" . $lang . "/header.php");
+include(__DIR__ . "/dark/" . $lang . "/" . $page . ".php");
+include(__DIR__ . "/dark/" . $lang . "/footer.php");
 
-<?php
-// import page !
-
-include("dark/" . $lang . "/header.php");
-include("dark/" . $lang . "/" . $page . ".php");
-include("dark/" . $lang . "/footer.php");
-
-if ($lang == ("EN/" or "EN")) {
+if ($lang === LANG_EN) {
 	include("en_sup.php");
 } else {
 	include("fr_sup.php");
 }
-?>
